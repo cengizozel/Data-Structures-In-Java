e03d16e3a125190dd9987b626fc38de91a65dc5d
cengizozel <60388555+cengizozel@users.noreply.github.com>
2020-12-12 16:47:13 -0500

    Added from Eclipse

---
+++ DataStructures/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		LinkedList list = new LinkedList();
+		list.insert(1);
+		list.insert(2);
+		list.insert(3);
+		list.insertAtStart(0);
+		list.insertAt(2, 15);
+		list.insertAt(0, -1);
+		list.show();
+		
+		list.deleteAt(2);
+		list.show();
+		*/
+		
+		/*
+		Stack stack = new Stack();
+		System.out.println("Stack is empty: " + stack.isEmpty());
+		
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.show();
+		System.out.println("Stack is empty: " + stack.isEmpty());
+		
+		stack.pop();
+		stack.show();
+		
+		stack.push(4);
+		stack.show();
+		System.out.println("Peek is " + stack.peek());
+		System.out.println("Size is " + stack.size());
+		
+		stack.push(5);
+		stack.push(6);
+		stack.push(10);
+		stack.show();
+		
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.show();
+		*/
+	}
+	
+}
